11abcbb8ad40b6fb230a2ce898e754ff85254aeb
Rolf Dramdal <rolf.dramdal@miles.no>
2022-03-30 08:29:17 +0200

    dotnet kafka consumer/producer example applications

---
+++ examples/dotnet-kafka-consumer/KafkaConsumer/Services/KafkaMessageConsumer.cs
@@ -0,0 +1,93 @@
+﻿
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaConsumer.Services
+{
+    public class KafkaMessageConsumer : BackgroundService
+    {
+        public static List<MessageDTO> resultList;
+        private readonly IConfiguration _config;
+        private readonly ILogger<KafkaMessageConsumer> _logger;
+
+        public KafkaMessageConsumer(IConfiguration config, ILogger<KafkaMessageConsumer> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.Run(() => ProcessQueue(stoppingToken), stoppingToken);
+
+        }
+
+        private ConsumeResult<Null, MessageDTO> ProcessQueue(CancellationToken stoppingToken)
+        {
+            var topic = _config.GetValue<string>("Kafka:Topic");
+            var bootstrapServer = _config.GetValue<string>("Kafka:BootstrapServer");
+
+            var config = new ConsumerConfig
+            {
+                GroupId = "dotnet-kafka",
+                BootstrapServers = bootstrapServer,
+                EnableSslCertificateVerification = false,
+                AutoOffsetReset = AutoOffsetReset.Latest,
+                EnableAutoCommit = false,
+            };
+            _logger.LogInformation($"Starting KafaConsumer on topic: '{topic}' with bootstrap server: '{bootstrapServer}'...");
+            using (var consumer = 
+                new ConsumerBuilder<Null, MessageDTO>(config)
+                .SetErrorHandler((_, e) =>
+                {
+                    _logger.LogError(e.ToString());
+                })
+                .SetPartitionsAssignedHandler((c, partitions) =>
+                {
+                    foreach (var partition in partitions)
+                    {
+                        _logger.LogInformation($"Consuming messages on partition: '{partition.Partition.Value}' topic: '{partition.Topic}'");
+                    }
+                })
+                .SetValueDeserializer(new MessageDTODeSerializer())
+                .Build())
+            {
+                consumer.Subscribe(topic);
+                try
+                {
+                    if (resultList == null)
+                    {
+                        resultList = new List<MessageDTO>();
+                    }
+                    while(!stoppingToken.IsCancellationRequested)
+                    {
+                        var result = consumer.Consume(stoppingToken);
+                        _logger.LogInformation($"Consumed message '{result.Message.Value.Message}' on partition: '{result.Partition.Value}' at offset: '{result.TopicPartitionOffset.Offset}'");
+                        if(stoppingToken.IsCancellationRequested)
+                        {
+                            _logger.LogWarning("Kafka consumer stopped");
+                        }
+                    }
+                }
+                catch (ConsumeException ex)
+                {
+                    _logger.LogError($"Error occured: {ex.Error.Reason}");
+                    consumer.Close();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error occured: {ex.StackTrace}");
+                    consumer.Close();
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ examples/dotnet-kafka-producer/KafkaProducer/Controllers/MessageController.cs
@@ -0,0 +1,42 @@
+﻿using KafkaProducer.Infrastrutcure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace KafkaProducer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private readonly ILogger<MessageController> _logger;
+        private readonly IKafkaMessageProducer _producer;
+
+        public MessageController(ILogger<MessageController> logger, IKafkaMessageProducer producer)
+        {
+            _logger = logger;
+            _producer = producer;
+        }
+
+        [HttpPost("async")]
+        [Consumes("application/json")]
+        public async Task<ActionResult> SendMessageAsync(MessageDTO message)
+        {
+            _logger.LogDebug($"Received message: {message.ToJson()}");
+            var messageResult = await _producer.SendMessageAsync(message);
+
+            return Accepted(messageResult);
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        public ActionResult SendMessage(MessageDTO message)
+        {
+            _logger.LogDebug($"Received message: {message.ToJson()}");
+            _producer.SendMessage(message);
+
+            return Accepted();
+        }
+    }
+}
