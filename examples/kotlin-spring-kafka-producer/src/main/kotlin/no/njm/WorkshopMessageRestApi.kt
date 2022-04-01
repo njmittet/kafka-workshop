bc75a80d0f2cea75a8b090e85b7032e11c593a0a
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-04-01 08:46:44 +0200

    Bump Spring Boot to 2.6.6

--- examples/kotlin-spring-kafka-producer/src/main/kotlin/no/njm/WorkshopMessageRestApi.kt
+++ examples/kotlin-spring-kafka-producer/src/main/kotlin/no/njm/WorkshopMessageRestApi.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
+import java.util.UUID
 
 @RestController
 class WorkshopMessageRestApi(
